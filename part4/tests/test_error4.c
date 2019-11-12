425c4bf337fb5d26990008173e0145a3b3b5d4f6
Aendur <Aendur@users.noreply.github.com>
2019-11-11 21:47:05 -0300

    Create test_error4.c

---
+++ part4/tests/test_error4.c
@@ -0,0 +1,32 @@
+int min(int x, int y) {
+	int x = y;
+	int y = x;
+	{
+		int x = y;
+		int y = x;
+	}
+	return 0;
+}
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+/*                                  */
